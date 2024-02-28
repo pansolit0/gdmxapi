2e21be827f5e5fefe42843fea4e9e5f7847fbfe6
Francisco Mancilla <137464457+pansolit0@users.noreply.github.com>
2024-02-28 08:11:06 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {obtenerDatosChart,exportarExcel,promedioCatorceDias} = require('../controllers/dataController');
+const { loginUsuario } = require('../controllers/authController')
+
 
 
 // Definición de la ruta para obtener datos
 router.post('/datosChart', obtenerDatosChart); //Funcion dentro de la API PHP: apichart
 router.post('/exportExcel', exportarExcel); //Funcion dentro de la API PHP: apiExport
 router.post('/promedioCatorce', promedioCatorceDias); //Funcion dentro de la API PHP: Apirestful
+
+
+router.post('/users/login',loginUsuario);
 
 module.exports = router; 
