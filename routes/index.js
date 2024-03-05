9024c343954e01ace53f5f1d32298a95ca8a5d0c
Francisco Mancilla <137464457+pansolit0@users.noreply.github.com>
2024-03-05 08:08:49 -0400

    se agregaron rutas a las funciones de la datacontroller.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {obtenerDatosChart,exportarExcel,promedioCatorceDias} = require('../controllers/dataController');
+const {obtenerDatosChart,exportarExcel,promedioCatorceDias, obtenerDatosChartGrafico, obtenerDatosTabla} = require('../controllers/dataController');
 const { loginUsuario } = require('../controllers/authController')
-
 
 
 // Definición de la ruta para obtener datos
 router.post('/datosChart', obtenerDatosChart); //Funcion dentro de la API PHP: apichart
 router.post('/exportExcel', exportarExcel); //Funcion dentro de la API PHP: apiExport
 router.post('/promedioCatorce', promedioCatorceDias); //Funcion dentro de la API PHP: Apirestful
+router.post('/grafico', obtenerDatosChartGrafico);
+router.post('/tabla', obtenerDatosTabla);
 
 
 router.post('/users/login',loginUsuario);
